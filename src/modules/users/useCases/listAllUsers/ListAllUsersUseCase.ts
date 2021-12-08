72b8c9c52eb35f07c7e7b91dddd3c8b2fa54b68e
João Victor P. Rodrigues <jvpr123@outlook.com>
2021-12-08 16:17:41 -0300

    Use cases and controllers implemented - passed tests.

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("mensagem de erro");
+    } else if (!user.admin) {
+      throw new Error("mensagem de erro");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,14 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.params;
+
+    try {
+      const userExists = this.showUserProfileUseCase.execute({ user_id });
+      return response.status(200).json(userExists);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
